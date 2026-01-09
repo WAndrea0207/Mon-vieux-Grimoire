f03a050e5d03ee0993d04500b0cf5a4451a44772
WAndrea0207 <firexdarkx@gmail.com>
2026-01-09 18:50:49 +0100

    add CRUD app

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Book = require('./models/book');
 
 const app = express();
 
@@ -8,7 +9,7 @@ mongoose.connect('mongodb+srv://will:grimoire@cluster0.in5va.mongodb.net/monvieu
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// Active les requêtes depuis le frontend React
+// CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -25,16 +26,41 @@ app.get('/api/test', (req, res) => {
 
 // Route pour récupérer les livres
 app.get('/api/books', (req, res) => {
-  res.json({ books: [] });
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
 });
 
-// Route pour créer un livre
+// GET - Récupérer UN SEUL livre par ID
+app.get('/api/books/:id', (req, res) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+});
+
+//  PUT - Modifier un livre
+app.put('/api/books/:id', (req, res) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre modifié !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+//  DELETE - Supprimer un livre
+app.delete('/api/books/:id', (req, res) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// POST - Créer un nouveau livre
 app.post('/api/books', (req, res) => {
-  console.log('Nouveau livre reçu :', req.body);
-  res.status(201).json({
-    message: 'Livre créé avec succès !',
-    book: req.body,
+  delete req.body._id;
+  const book = new Book({
+    ...req.body
   });
+  book.save()
+    .then(() => res.status(201).json({ message: 'Livre enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
