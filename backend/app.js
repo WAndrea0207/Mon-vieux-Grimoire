0dfb304162b9be1a9ada2e705fe30b62debdde45
WAndrea0207 <firexdarkx@gmail.com>
2026-01-09 17:56:53 +0100

    fix server code + initialize db

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Connexion à MongoDB
+mongoose.connect('mongodb+srv://will:grimoire@cluster0.in5va.mongodb.net/monvieuxgrimoire?appName=Cluster0',)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 // Active les requêtes depuis le frontend React
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
-});
+}); 
 
 app.use(express.json());
 
@@ -19,6 +26,15 @@ app.get('/api/test', (req, res) => {
 // Route pour récupérer les livres
 app.get('/api/books', (req, res) => {
   res.json({ books: [] });
+});
+
+// Route pour créer un livre
+app.post('/api/books', (req, res) => {
+  console.log('Nouveau livre reçu :', req.body);
+  res.status(201).json({
+    message: 'Livre créé avec succès !',
+    book: req.body,
+  });
 });
 
 module.exports = app;
