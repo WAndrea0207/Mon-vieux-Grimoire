5bb047101879c6c57518946236f08b8637a19c82
WAndrea0207 <firexdarkx@gmail.com>
2026-01-23 11:09:59 +0100

    remove auth middleware from get routes in bookRoute

--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -6,8 +6,8 @@ const multer = require('../middleware/multer-config');
 const bookController = require('../controllers/bookController');
 
 // Routes CRUD
-router.get('/', auth, bookController.getAllBooks);
-router.get('/:id', auth, bookController.getOneBook);
+router.get('/', bookController.getAllBooks);
+router.get('/:id', bookController.getOneBook);
 router.post('/', auth, multer, bookController.createBook);
 router.put('/:id', auth, multer, bookController.updateBook);  
 router.delete('/:id', auth, bookController.deleteBook);
